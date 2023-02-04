8e38e8b6ac34a50252a5fe1a6edf22525dcd3168
Alan <alannhan@ymail.com>
2023-02-04 15:31:37 -0800

    Attempting backend dev

--- script.js
+++ script.js
@@ -1,48 +1,3 @@
-// const recipeCardTemplate = document.querySelector("[data-recipe-template]");
-// const userCardContainer = document.querySelector(
-//   "[data-recipe-cards-container]"
-// );
-// const searchInput = document.querySelector("[data-search]");
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '1ceed61d7cmshbb898d593945536p104570jsn6c6039b6fd9b',
-// 		'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-// 	}
-// };
-
-// let recipes = [];
-
-// searchInput.addEventListener("input", (e) => {
-//   const value = e.target.value.toLowerCase();
-//   recipes.forEach((recipe) => {
-//     const isVisible =
-//       recipe.name.includes(value) || recipe.email.includes(value);
-//     recipe.element.classList.toggle("hide", !isVisible);
-//   });
-// });
-
-// // API WORKS
-// // user is switched with recipe and be sure to align with API
-// fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes', options)
-// 	.then(response => response.json())
-//   .then(data => {
-// 	// .then(response => console.log(response))
-// 	// .catch(err => console.error(err));
-//   //   recipes = data.map((recipe) => {
-//       const card = recipeCardTemplate.content.cloneNode(true).children[0];
-//       const header = card.querySelector("[data-header]");
-//       const body = card.querySelector("[data-body]");
-//       header.textContent = recipes.list; // FIX parameter
-//       body.textContent = recipes.list-similarities; // FIX parameter
-//       userCardContainer.append(card);
-//       return { // FIX THIS - data needs to match with API
-//         name: recipes.name, 
-//         ingredients: recipes.list,
-//         element: card,
-//       };
-//     });
-
 const options = {
 	method: 'GET',
 	headers: {
@@ -51,7 +6,30 @@ const options = {
 	}
 };
 
-fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes', options)
+// formtest is just for testing
+const formtest = document.querySelector("form");
+
+formtest.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const searchTerm = document.querySelector("#searchTerm").value;
+
+  fetchRecipes(searchTerm);
+});
+
+fetch('https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=chicken%20soup', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
+
+function displayRecipes(recipes) {
+  const recipeList = document.querySelector("#recipe-list");
+  recipeList.innerHTML = "";
+
+  recipes.forEach((recipe) => {
+    const li = document.createElement("li");
+    li.innerHTML = `<h3>${recipe.recipe.label}</h3>
+                    <img src="${recipe.recipe.image}" alt="${recipe.recipe.label}">`;
+    recipeList.appendChild(li);
+  });
+}
